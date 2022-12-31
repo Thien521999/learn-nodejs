bd33863a1a5040ec7453857f85eb7a0d05a2b3d1
Martin Tran <martin.tran@legato.co>
2022-12-31 22:58:05 +0700

    routing aand MVC

--- src/server.js
+++ src/server.js
@@ -1,20 +1,16 @@
 import express from "express";
 import configViewEngine from './configs/viewEngine';
+import initWebRoute from './route/web';
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 8080;
-console.log('--port', port);
 
+// setup view engine
 configViewEngine(app);
 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
-})
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// init web route
+initWebRoute(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
