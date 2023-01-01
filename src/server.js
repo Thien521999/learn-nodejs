7d752aeec381255b7dd0b9dca5af78f8290fb441
Martin Tran <martin.tran@legato.co>
2023-01-02 00:38:22 +0700

    render data in view

--- src/server.js
+++ src/server.js
@@ -1,16 +1,24 @@
 import express from "express";
+import connectDB from "./config/connectDB";
 import configViewEngine from './config/viewEngine';
 import initWebRoute from './route/web';
+// import connectDB from 'connectDB';
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 8080;
+
+// config app
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 // setup view engine
 configViewEngine(app);
-
 // init web route
 initWebRoute(app);
+
+connectDB();
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
