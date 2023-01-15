31243f676dfd2c39265c9e1339a8fb54fbcafb34
Martin Tran <martin.tran@legato.co>
2023-01-15 10:30:56 +0700

    write api handle login

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from "../controller/homeController";
+import userController from "../controller/userController";
 
 let router = express.Router();
 
@@ -7,12 +8,13 @@ const initWebRoute = (app) => {
   router.get("/", homeController.getHomePage);
   router.get("/about", homeController.getAboutPage);
   router.get("/crud", homeController.getCRUD);
-
   router.post("/post-crud", homeController.postCRUD);
   router.get("/get-crud", homeController.diplayGetCRUD);
   router.get("/edit-crud", homeController.getEditCRUD);
   router.post("/put-crud", homeController.putCRUD);
   router.get("/delete-crud", homeController.deleteCRUD);
+
+  router.post("/api/login", userController.handleLogin);
 
   return app.use("/", router);
 };
